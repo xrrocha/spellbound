8ee1217b3abc264468c7da5c5bcfdac5d85a9e72
Ricardo Rocha <ricardo@xrrocha.net>
2018-02-08 19:52:10 -0500

    Removed unneeded Python script

--- spellbound-norvig/data/FindCorrections.py
+++
@@ -1,8 +0,0 @@
-import re
-from NorvigSpellingCorrector import candidates
-
-lines = [line.rstrip('\n') for line in open('spanish-name-typos.tsv')]
-for line in lines:
-    typo, word = re.split(r'\t+', line)
-    for candidate in candidates(typo):
-        print(typo + "\t" + candidate)
