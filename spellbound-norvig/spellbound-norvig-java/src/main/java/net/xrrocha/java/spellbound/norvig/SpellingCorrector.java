a855eb64030866e16c143ca9fd9c45e570f33147
Ricardo Rocha <ricardo@xrrocha.net>
2018-02-08 19:53:15 -0500

    Added first functional implementation of Norvig's spelling corrector

--- spellbound-norvig/spellbound-norvig-java/src/main/java/net/xrrocha/java/spellbound/norvig/SpellingCorrector.java
+++ spellbound-norvig/spellbound-norvig-java/src/main/java/net/xrrocha/java/spellbound/norvig/SpellingCorrector.java
@@ -1,16 +1,30 @@
 package net.xrrocha.java.spellbound.norvig;
 
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
+import java.util.*;
+import java.util.function.Function;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 import static com.google.common.base.Preconditions.checkArgument;
 import static com.google.common.base.Preconditions.checkNotNull;
+import static java.util.Collections.emptyList;
 
 public class SpellingCorrector {
 
   private final Map<String, Integer> dictionary;
+
+  static final Pattern ALPHABETIC = Pattern.compile("^[a-z]+$");
+
+  static final String[] LETTERS = "abcdefghijklmnopqrstuvwxyz".split("");
+
+  static final List<Function<List<Split>, List<String>>> edits = List.of(
+      SpellingCorrector::deletes,
+      SpellingCorrector::transposes,
+      SpellingCorrector::replaces,
+      SpellingCorrector::inserts
+  );
 
   public SpellingCorrector(Map<String, Integer> dictionary) {
     checkNotNull(dictionary);
@@ -18,16 +32,98 @@ public class SpellingCorrector {
     this.dictionary = dictionary;
   }
 
-  static final Pattern ALPHABETIC = Pattern.compile("^[\\p{Alpha}]+$");
-
   public Optional<List<String>> getCorrections(String word) {
+
     String normalizedWord = normalize(word);
 
     if (dictionary.containsKey(normalizedWord)) {
       return Optional.empty();
     }
 
-    throw new UnsupportedOperationException("Unimplemented: getCorrections()");
+    final Optional<List<String>> corrections;
+
+    List<String> corrections1 = edits1(normalizedWord);
+
+    if (corrections1.isEmpty()) {
+      List<String> corrections2 = edits2(normalizedWord);
+      if (corrections2.isEmpty()) {
+        corrections = Optional.of(emptyList());
+      } else {
+        corrections = Optional.of(corrections2);
+      }
+    } else {
+      corrections = Optional.of(corrections1);
+    }
+
+    return corrections;
+  }
+
+  List<String> edits1(String word) {
+
+    List<Split> wordSplits = splits(word);
+
+    return pack(edits.stream()
+        .flatMap(edit -> edit.apply(wordSplits).stream()));
+  }
+
+  List<String> edits2(String word) {
+    return
+        pack(edits1(word).stream()
+            .flatMap(w -> edits1(w).stream()));
+  }
+
+  List<String> pack(Stream<String> stream) {
+    return stream
+        .distinct()
+        .filter(dictionary::containsKey)
+        .sorted(Comparator.comparing(dictionary::get))
+        .collect(Collectors.toList());
+  }
+
+  static List<Split> splits(String word) {
+    return IntStream
+        .rangeClosed(0, word.length()).boxed()
+        .map(i -> {
+          String left = word.substring(0, i);
+          String right = word.substring(i);
+          return new Split(left, right);
+        })
+        .collect(Collectors.toList());
+  }
+
+  static List<String> deletes(List<Split> splits) {
+    return splits.stream()
+        .filter(split -> !split.right.isEmpty())
+        .map(split -> split.left + split.right.substring(1))
+        .collect(Collectors.toList());
+  }
+
+  static List<String> transposes(List<Split> splits) {
+    return splits.stream()
+        .filter(split -> split.right.length() > 1)
+        .map(split -> split.left + split.right.substring(1, 2) + split.right.substring(0, 1) + split.right.substring(2))
+        .collect(Collectors.toList());
+  }
+
+  static List<String> replaces(List<Split> splits) {
+    return splits.stream()
+        .filter(split -> !split.right.isEmpty())
+        .flatMap(split ->
+            Arrays.stream(LETTERS).map(letter ->
+                split.left + letter + split.right.substring(1)
+            )
+        )
+        .collect(Collectors.toList());
+  }
+
+  static List<String> inserts(List<Split> splits) {
+    return splits.stream()
+        .flatMap(split ->
+            Arrays.stream(LETTERS).map(letter ->
+                split.left + letter + split.right
+            )
+        )
+        .collect(Collectors.toList());
   }
 
   static String normalize(String word) {
@@ -40,5 +136,26 @@ public class SpellingCorrector {
   static boolean isAlphabetic(String word) {
     checkNotNull(word);
     return ALPHABETIC.matcher(word).matches();
+  }
+
+  static class Split {
+    final String left;
+    final String right;
+
+    public Split(String left, String right) {
+      checkNotNull(left);
+      checkNotNull(right);
+      this.left = left;
+      this.right = right;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+      if (obj == null || !(obj instanceof Split)) {
+        return false;
+      }
+      Split that = (Split) obj;
+      return this.left.equals(that.left) && this.right.equals(that.right);
+    }
   }
 }
--- spellbound-norvig/spellbound-norvig-java/src/test/java/net/xrrocha/java/spellbound/norvig/SpellingCorrectorTest.java
+++ spellbound-norvig/spellbound-norvig-java/src/test/java/net/xrrocha/java/spellbound/norvig/SpellingCorrectorTest.java
@@ -1,58 +1,210 @@
 package net.xrrocha.java.spellbound.norvig;
 
-import org.junit.Ignore;
+import net.xrrocha.java.spellbound.norvig.SpellingCorrector.Split;
 import org.junit.Test;
 
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
+import java.util.stream.IntStream;
 
 import static junit.framework.TestCase.fail;
-import static org.junit.Assert.*;
+import static net.xrrocha.java.spellbound.norvig.SpellingCorrector.LETTERS;
+import static net.xrrocha.java.spellbound.norvig.SpellingCorrector.deletes;
+import static net.xrrocha.java.spellbound.norvig.SpellingCorrector.inserts;
+import static net.xrrocha.java.spellbound.norvig.SpellingCorrector.isAlphabetic;
+import static net.xrrocha.java.spellbound.norvig.SpellingCorrector.normalize;
+import static net.xrrocha.java.spellbound.norvig.SpellingCorrector.replaces;
+import static net.xrrocha.java.spellbound.norvig.SpellingCorrector.splits;
+import static net.xrrocha.java.spellbound.norvig.SpellingCorrector.transposes;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 public class SpellingCorrectorTest {
 
-  private final SpellingCorrector defaultCorrector = new SpellingCorrector(Map.of("spelling", 1));
+  private final SpellingCorrector spellingCorrector = new SpellingCorrector(Map.of(
+      "centry", 93832,
+      "contra", 13242,
+      "country", 393,
+      "ricksha", 1000000,
+      "sleeping", 5216,
+      "sliping", 1000000,
+      "sloping", 27280,
+      "spelling", 7302,
+      "spewing", 41780,
+      "spiling", 1000000
+  ));
+
+  @Test
+  public void yieldsEmptyOnDictionaryWord() {
+    assertFalse(spellingCorrector.getCorrections("spelling").isPresent());
+  }
+
+  @Test
+  public void yieldsNonEmptyCorrectionsOnTypo() {
+    List<String> expectedCorrections = List.of(
+        "spelling", "spewing", "spiling"
+    );
+    Optional<List<String>> actualCorrections = spellingCorrector.getCorrections("speling");
+    assertTrue(actualCorrections.isPresent());
+    assertTrue(equals(expectedCorrections, actualCorrections.get()));
+  }
+
+  @Test
+  public void yieldsEmptyCorrectionsOnGibberish() {
+    Optional<List<String>> corrections = spellingCorrector.getCorrections("xwphjwl");
+    assertTrue(corrections.isPresent());
+    assertTrue(corrections.get().isEmpty());
+  }
+
+  @Test
+  public void buildsSplitsCorrectly() {
+    String name = "dilbert";
+
+    List<Split> expectedSplits = List.of(
+        new Split("", "dilbert"),
+        new Split("d", "ilbert"),
+        new Split("di", "lbert"),
+        new Split("dil", "bert"),
+        new Split("dilb", "ert"),
+        new Split("dilbe", "rt"),
+        new Split("dilber", "t"),
+        new Split("dilbert", "")
+    );
+
+    List<Split> actualSplits = splits(name);
+
+    assertEquals(name.length() + 1, actualSplits.size());
+
+    assertTrue(equals(expectedSplits, actualSplits));
+  }
+
+  @Test
+  public void buildsDeletesCorrectly() {
+    String name = "wally";
+    List<Split> splits = splits(name);
+
+    List<String> expectedDeletes = List.of(
+        "ally", "wlly", "waly", "waly", "wall"
+    );
+    List<String> actualDeletes = deletes(splits);
+    assertEquals(actualDeletes.size(), name.length());
+
+    assertTrue(equals(expectedDeletes, actualDeletes));
+  }
+
+  @Test
+  public void buildsTransposesCorrectly() {
+    String name = "alice";
+    List<Split> splits = splits(name);
+
+    List<String> expectedTransposes = List.of(
+        "laice", "ailce", "alcie", "aliec"
+    );
+    List<String> actualTransposes = transposes(splits);
+    assertEquals(actualTransposes.size(), name.length() - 1);
+
+    assert (equals(expectedTransposes, actualTransposes));
+  }
+
+  @Test
+  public void buildsReplacesCorrectly() {
+    String name = "asok";
+    List<Split> splits = splits(name);
+
+    List<String> expectedReplaces = List.of(
+        "asok", "bsok", "csok", "dsok", "esok", "fsok", "gsok", "hsok",
+        "isok", "jsok", "ksok", "lsok", "msok", "nsok", "osok", "psok",
+        "qsok", "rsok", "ssok", "tsok", "usok", "vsok", "wsok", "xsok",
+        "ysok", "zsok", "aaok", "abok", "acok", "adok", "aeok", "afok",
+        "agok", "ahok", "aiok", "ajok", "akok", "alok", "amok", "anok",
+        "aook", "apok", "aqok", "arok", "asok", "atok", "auok", "avok",
+        "awok", "axok", "ayok", "azok", "asak", "asbk", "asck", "asdk",
+        "asek", "asfk", "asgk", "ashk", "asik", "asjk", "askk", "aslk",
+        "asmk", "asnk", "asok", "aspk", "asqk", "asrk", "assk", "astk",
+        "asuk", "asvk", "aswk", "asxk", "asyk", "aszk", "asoa", "asob",
+        "asoc", "asod", "asoe", "asof", "asog", "asoh", "asoi", "asoj",
+        "asok", "asol", "asom", "ason", "asoo", "asop", "asoq", "asor",
+        "asos", "asot", "asou", "asov", "asow", "asox", "asoy", "asoz"
+    );
+    List<String> actualReplaces = replaces(splits);
+    assertEquals(actualReplaces.size(), LETTERS.length * name.length());
+
+    assertTrue(equals(expectedReplaces, actualReplaces));
+  }
+
+  @Test
+  public void buildsInsertsCorrectly() {
+    String name = "asok";
+    List<Split> splits = splits(name);
+
+    List<String> expectedInserts = List.of(
+        "aasok", "basok", "casok", "dasok", "easok", "fasok", "gasok",
+        "hasok", "iasok", "jasok", "kasok", "lasok", "masok", "nasok",
+        "oasok", "pasok", "qasok", "rasok", "sasok", "tasok", "uasok",
+        "vasok", "wasok", "xasok", "yasok", "zasok", "aasok", "absok",
+        "acsok", "adsok", "aesok", "afsok", "agsok", "ahsok", "aisok",
+        "ajsok", "aksok", "alsok", "amsok", "ansok", "aosok", "apsok",
+        "aqsok", "arsok", "assok", "atsok", "ausok", "avsok", "awsok",
+        "axsok", "aysok", "azsok", "asaok", "asbok", "ascok", "asdok",
+        "aseok", "asfok", "asgok", "ashok", "asiok", "asjok", "askok",
+        "aslok", "asmok", "asnok", "asook", "aspok", "asqok", "asrok",
+        "assok", "astok", "asuok", "asvok", "aswok", "asxok", "asyok",
+        "aszok", "asoak", "asobk", "asock", "asodk", "asoek", "asofk",
+        "asogk", "asohk", "asoik", "asojk", "asokk", "asolk", "asomk",
+        "asonk", "asook", "asopk", "asoqk", "asork", "asosk", "asotk",
+        "asouk", "asovk", "asowk", "asoxk", "asoyk", "asozk", "asoka",
+        "asokb", "asokc", "asokd", "asoke", "asokf", "asokg", "asokh",
+        "asoki", "asokj", "asokk", "asokl", "asokm", "asokn", "asoko",
+        "asokp", "asokq", "asokr", "asoks", "asokt", "asoku", "asokv",
+        "asokw", "asokx", "asoky", "asokz"
+    );
+    List<String> actualInserts = inserts(splits);
+    assertEquals(actualInserts.size(), LETTERS.length * (name.length() + 1));
+
+    assertTrue(equals(expectedInserts, actualInserts));
+  }
 
   @Test(expected = NullPointerException.class)
   public void rejectsNullAlphabetic() {
-    defaultCorrector.isAlphabetic(null);
+    isAlphabetic(null);
   }
 
   @Test
   public void recognizesNonAlphabetic() {
-    assertFalse(defaultCorrector.isAlphabetic("neo42"));
+    assertFalse(isAlphabetic("neo42"));
   }
 
   @Test
   public void acceptsAlphabetic() {
-    defaultCorrector.isAlphabetic("neo");
-    defaultCorrector.isAlphabetic("Neo");
-    defaultCorrector.isAlphabetic("NEO");
+    isAlphabetic("neo");
+    isAlphabetic("Neo");
+    isAlphabetic("NEO");
   }
 
   @Test(expected = NullPointerException.class)
   public void rejectsNullWord() {
-    defaultCorrector.getCorrections(null);
+    spellingCorrector.getCorrections(null);
   }
 
   @Test(expected = NullPointerException.class)
   public void rejectsNullNormalization() {
-    defaultCorrector.normalize(null);
+    normalize(null);
   }
 
   @Test(expected = IllegalArgumentException.class)
   public void rejectsNonAlphaNormalization() {
-    defaultCorrector.normalize("!@#$");
+    normalize("!@#$");
   }
 
   @Test
   public void acceptsAlphaNormalization() {
-    String normalizedWord = defaultCorrector.normalize(" Neo\t\n");
+    String normalizedWord = normalize(" Neo\t\n");
     assertEquals("neo", normalizedWord);
   }
 
-  @Test(expected = IllegalArgumentException.class)
+  @Test(expected = NullPointerException.class)
   public void rejectsNullDictionary() {
     new SpellingCorrector(null);
   }
@@ -72,7 +224,7 @@ public class SpellingCorrectorTest {
     );
     nonAlphas.forEach(badWord -> {
       try {
-        defaultCorrector.getCorrections(badWord);
+        spellingCorrector.getCorrections(badWord);
         fail("Accepts non-alpha: " + badWord);
       } catch (IllegalArgumentException | NullPointerException e) {
       }
@@ -81,27 +233,12 @@ public class SpellingCorrectorTest {
 
   @Test(expected = NullPointerException.class)
   public void rejectsNullWords() {
-    defaultCorrector.getCorrections(null);
+    spellingCorrector.getCorrections(null);
   }
 
-  @Test
-  public void yieldsEmptyOnDictionaryWord() {
-    assertFalse(defaultCorrector.getCorrections("spelling").isPresent());
-  }
-
-  @Test
-  @Ignore
-  public void yieldsNonEmptyCorrectionsOnTypo() {
-    Optional<List<String>> corrections = defaultCorrector.getCorrections("speling");
-    assertTrue(corrections.isPresent());
-    assertTrue(corrections.get().size() > 0);
-  }
-
-  @Test
-  @Ignore
-  public void yieldsEmptyCorrectionsOnGibberish() {
-    Optional<List<String>> corrections = defaultCorrector.getCorrections("xwphjwl");
-    assertTrue(corrections.isPresent());
-    assertTrue(corrections.get().isEmpty());
+  static <T> boolean equals(List<T> s1, List<T> s2) {
+    assertEquals(s1.size(), s2.size());
+    return IntStream.range(0, s1.size())
+        .allMatch(i -> s1.get(i).equals(s2.get(i)));
   }
 }
